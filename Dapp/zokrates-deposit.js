a34a9846b1ee740aada6b0aa07a31c8a79eb0f4f
GoshgarIsmayilov <47892295+GoshgarIsmayilov@users.noreply.github.com>
2023-11-11 21:51:57 +0300

    Updating Dapp-withdraw

---
+++ Dapp/zokrates-deposit.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:687b4e751c8bcf49f84b11fcce65d5422f324db9ab982ac5d21401bbc5f9d06d
+size 45259678
---
+++ Dapp/zokrates-withdraw.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d2c5c86dbea20a21b082425761f2d1dc52d05931bd2fd2813778bb87cfb477f
+size 45259666
